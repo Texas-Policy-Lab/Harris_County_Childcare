80d43b23ced247e10d874805632f810b4524d7e4
Joli Holmes <jh111@ad.rice.edu>
2020-04-17 12:28:32 -0500

    removed dependencies to put them in their own folder

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import pandas as pd
 import numpy as np
-import yaml
 
-from selenium import webdriver 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.common.exceptions import TimeoutException
-
-def read_yaml(fl = "config.yaml"):
-    with open(fl, 'r') as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            return exc
+from funcs.utils import read_yaml
+from funcs.driver_functions import swebdriver
 
 config = read_yaml()
 
 zip = pd.read_csv("./data/uszips.csv")
 zip = zip[zip['county_fips'].isin(config['fips'])]
+
+driver = swebdriver()
+navigate_page()
 
 import pdb; pdb.set_trace()
