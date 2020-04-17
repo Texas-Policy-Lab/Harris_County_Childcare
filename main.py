19ae6780d2782eed9814668a71be1a9f68ecb61b
Joli Holmes <jh111@ad.rice.edu>
2020-04-17 11:58:34 -0500

    reordered dependencies

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import numpy as np
 import yaml
+
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
 
 def read_yaml(fl = "config.yaml"):
     with open(fl, 'r') as stream:
